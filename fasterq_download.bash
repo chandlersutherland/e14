a6ab19aa09ba7c31c80d4468ac73107f6166be7c
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-08-23 13:43:01 -0700

    make rna and bisulfite variables

--- fasterq_download.bash
+++ fasterq_download.bash
@@ -18,10 +18,10 @@ module load sra-tools
 #download 1001 epigenomes methylc and rna 
 SCRATCH_DIR=/global/scratch/users/chandlersutherland/e14/
 
-echo "beginning ecker" 
+#echo "beginning ecker" 
 
-fasterq-dump --threads $SLURM_NTASKS -O $SCRATCH_DIR/bs_fastq_files/ecker -t $SCRATCH_DIR -p SRR771698
-fasterq-dump --threads $SLURM_NTASKS -O $SCRATCH_DIR/rna_fastq_files/ecker -t $SCRATCH_DIR -p SRR3465232
+#fasterq-dump --threads $SLURM_NTASKS -O $SCRATCH_DIR/bs_fastq_files/ecker -t $SCRATCH_DIR -p SRR771698
+#fasterq-dump --threads $SLURM_NTASKS -O $SCRATCH_DIR/rna_fastq_files/ecker -t $SCRATCH_DIR -p SRR3465232
 
 echo "ecker finished, beginning williams" 
 
@@ -29,14 +29,14 @@ echo "ecker finished, beginning williams"
 bisulfite='SRR17281088 SRR17281087 SRR17281086 SRR17281085'
 rna='SRR17281236 SRR17281235 SRR17281234 SRR17281233'
 
-for i in bisulfite
+for i in $bisulfite
 	do 
 	fasterq-dump --threads $SLURM_NTASKS -O $SCRATCH_DIR/bs_fastq_files/williams -t $SCRATCH_DIR -p $i
 done 
 
 echo "finished williams bisulfite, beginning RNA"
 
-for i in rna
+for i in $rna
 	do 
 	fasterq-dump --threads $SLURM_NTASKS -O $SCRATCH_DIR/bs_fastq_files/williams -t $SCRATCH_DIR -p $i
 done
