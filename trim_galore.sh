c9e09ef52719d310b71cc7bc31101ccbcdce3018
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-08-26 16:11:49 -0700

    paired not paired file

--- trim_galore.sh
+++ trim_galore.sh
@@ -21,5 +21,5 @@ cd /global/scratch/users/chandlersutherland/e14/bs_fastq_files/williams/
 
 for f in $bisulfite
 do 
-   $TRIM_DIR/trim_galore -o $OUT_DIR --fastqc --illumina --paired_file "${f}"_1.fastq "${f}"_2.fastq
+   $TRIM_DIR/trim_galore -o $OUT_DIR --fastqc --illumina --paired "${f}"_1.fastq "${f}"_2.fastq
 done 
