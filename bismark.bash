83e4a4bfd25212e92368ded49316f653516448a5
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-08-29 17:37:31 -0700

    bismarking

---
+++ bismark.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=bismark
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=06:00:00
+#SBATCH --mail-user=chandlersutherland@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
+#SBATCH --output=/global/home/users/chandlersutherland/slurm_stdout/slurm-%j.out
+
+module load bowtie2
+module load samtools
+
+BISMARK=/global/home/users/chandlersutherland/programs/Bismark-0.23.0
+ARAPORT11=/global/scratch/users/chandlersutherland/phytozome/Athaliana/Araport11/assembly
+
+cd $BISMARK
+for f in /global/scratch/users/chandlersutherland/e14/trim_williams/*.fq
+	do
+	./bismark --genome $ARAPORT11  $f 
+done
+
+./bismark --genome $ARAPORT11 /global/scratch/users/chandlersutherland/e14/bs_fastq_files/ecker/SRR771698.fastq
