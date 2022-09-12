77079b5f867cfbbde2254c1c8f8d4554554e801e
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-09-12 10:44:13 -0700

    dem changes

--- STAR_genomedir.bash
+++ STAR_genomedir.bash
@@ -4,7 +4,7 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=01:00:00
+#SBATCH --time=00:10:00
 #SBATCH --mail-user=chandlersutherland@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
@@ -13,10 +13,28 @@
 module load python 
 source activate e14
 
-GENOME_DIR=/global/scratch/users/chandlersutherland/phytozome/Athaliana/Araport11/assembly/STAR_genome
+#The s
+GENOME_DIR_WILLIAMS=/global/scratch/users/chandlersutherland/phytozome/Athaliana/Araport11/assembly/STAR_genome_williams
+GENOME_DIR_ECKER=/global/scratch/users/chandlersutherland/phytozome/Athaliana/Araport11/assembly/STAR_genome_ecker
 FASTA_FILE=/global/scratch/users/chandlersutherland/phytozome/Athaliana/Araport11/assembly/Athaliana_447_TAIR10.fa
-GFF3_FILE=/global/scratch/users/chandlersutherland/phytozome/Athaliana/Araport11/assembly/Athaliana_447_Araport11.gene_exons.gff3
+GTF_FILE=/global/scratch/users/chandlersutherland/Athaliana/Araport11_GTF_genes_transposons.current.gtf
 
-STAR --runThreadN $SLURM_NTASKS --runMode genomeGenerate --genomeDir $GENOME_DIR --genomeFastaFile $FASTA_FILE --sjdbGTFtagExonParentTranscript $GFF3_FILE 
+STAR --runThreadN $SLURM_NTASKS \
+	 --runMode genomeGenerate \
+	 --genomeDir $GENOME_DIR_WILLIAMS \
+	 --genomeFastaFiles $FASTA_FILE \
+	 --genomeSAindexNbases 12 \
+	 --sjdbGTFfile $GTF_FILE  \
+	 --sjdbOverhang 49
 
-print('genome_dir made') 
+echo 'genome_dir_williams made' 
+
+STAR --runThreadN $SLURM_NTASKS \
+	 --runMode genomeGenerate \
+	 --genomeDir $GENOME_DIR_ECKER \
+	 --genomeFastaFiles $FASTA_FILE \
+	 --genomeSAindexNbases 12 \
+	 --sjdbGTFfile $GTF_FILE  \
+	 --sjdbOverhang 99
+
+echo 'genome_dir_ecker made' 
