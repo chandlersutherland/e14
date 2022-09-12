704c181518b7cd8793899aadd7bcd544434e4956
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-09-12 11:18:57 -0700

    /

--- STAR.bash
+++ STAR.bash
@@ -17,7 +17,7 @@ GENOME_DIR_WILLIAMS=/global/scratch/users/chandlersutherland/phytozome/Athaliana
 GENOME_DIR_ECKER=/global/scratch/users/chandlersutherland/phytozome/Athaliana/Araport11/assembly/STAR_genome_ecker
 ECKER=/global/scratch/users/chandlersutherland/e14/rna_fastq_files/ecker
 WILLIAMS=/global/scratch/users/chandlersutherland/e14/rna_fastq_files/williams
-OUTPUT=/global/scratch/users/chandlersutherland/e14/STAR_output/
+OUTPUT=/global/scratch/users/chandlersutherland/e14/STAR_output
 
 #start with ecker 
 STAR --runThreadN $SLURM_NTASKS --genomeDir $GENOME_DIR_ECKER --outFileNamePrefix $OUTPUT/SRR3465232_ --readFilesIn $ECKER/SRR3465232_1.fastq $ECKER/SRR3465232_2.fastq
@@ -30,7 +30,7 @@ cd $WILLIAMS
 echo 'starting williams'
 for file in $WILLIAMS 
 do 
-	STAR --runThreadN $SLURM_NTASKS --genomeDir $GENOME_DIR_WILLIAMS --outFileNamePrefix "${OUTPUT}"/"${file}" --readFilesIn $file
+	STAR --runThreadN $SLURM_NTASKS --genomeDir $GENOME_DIR_WILLIAMS --outFileNamePrefix "${OUTPUT}"/"${file}"_ --readFilesIn $file
 done 
 
 echo 'finished!' 
