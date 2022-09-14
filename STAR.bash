e16bbc1207d770014563e65b2c8beb5b0753434a
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-09-14 13:35:02 -0700

    oops I messed up the out prefix AGAIN

--- STAR.bash
+++ STAR.bash
@@ -32,20 +32,20 @@ echo 'starting williams'
 #didn't work, need to create basename variable in for loop for output prefix 
 RNA='SRR17281233  SRR17281234  SRR17281235  SRR17281236'
 
-cd $WILLIAMS
-for file in $RNA
-do 
-	BASENAME=$(basename $file .fastq)
-	STAR --runThreadN $SLURM_NTASKS --genomeDir $GENOME_DIR_WILLIAMS --outFileNamePrefix "${OUTPUT}"/"${BASENAME}"_ --readFilesIn ${file}.fastq
-done 
-
-echo 'finished!' 
-#
-#cd $WILLIAMS_TRIMMED
+#cd $WILLIAMS
 #for file in $RNA
 #do 
-#	BASENAME=$(basename $file)
-#	STAR --runThreadN $SLURM_NTASKS --genomeDir $GENOME_DIR_WILLIAMS --outFileNamePrefix "${OUTPUT}"/"${BASENAME#}"_trimmed --readFilesIn ${file}_trimmed.fq
+#	BASENAME=$(basename $file .fastq)
+#	STAR --runThreadN $SLURM_NTASKS --genomeDir $GENOME_DIR_WILLIAMS --outFileNamePrefix "${OUTPUT}"/"${BASENAME}"_ --readFilesIn ${file}.fastq
 #done 
+
+echo 'finished!' 
+
+cd $WILLIAMS_TRIMMED
+for file in $RNA
+do 
+	BASENAME=$(basename $file)
+	STAR --runThreadN $SLURM_NTASKS --genomeDir $GENOME_DIR_WILLIAMS --outFileNamePrefix "${OUTPUT}"/"${BASENAME}"_trimmed_ --readFilesIn ${file}_trimmed.fq
+done 
 
 echo 'finished!' 
