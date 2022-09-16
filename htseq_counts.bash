57414d8bc5c44d0367fbe137c429564793c800fe
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-09-15 23:10:21 -0700

    stranded + wallclock

--- htseq_counts.bash
+++ htseq_counts.bash
@@ -4,7 +4,7 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=01:00:00
+#SBATCH --time=03:00:00
 #SBATCH --mail-user=chandlersutherland@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
@@ -22,7 +22,7 @@ cd $INPUT
 for f in *.bam
 do 
 	BASENAME=$(basename $f .out.bam) 
-	htseq-count -r pos -s no \
+	htseq-count -r pos -s yes \
 		-c $OUTPUT/$BASENAME.txt 
 		$f \
 		/global/scratch/users/chandlersutherland/Athaliana/Araport11_GTF_genes_transposons.current.gtf
