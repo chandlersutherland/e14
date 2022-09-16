33f646e95097401654775b3237a38bf86b1e9eec
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-09-16 06:29:38 -0700

    loop through all NLRs

--- htseq_counts.bash
+++ htseq_counts.bash
@@ -19,11 +19,10 @@ OUTPUT=/global/scratch/users/chandlersutherland/e14/STAR_output/htseq_count
 
 cd $INPUT 
 
-#for f in *.bam
-f='SRR17281233_Aligned.out.bam'
-#do 
+for f in *.bam
+do 
 	BASENAME=$(basename $f _Aligned.out.bam) 
-	htseq-count -r pos -s yes $f /global/scratch/users/chandlersutherland/Athaliana/Araport11_GTF_genes_transposons.current.gtf
-
-#done
+	htseq-count -r pos -s yes -c $OUTPUT/$BASENAME.tsv $f /global/scratch/users/chandlersutherland/Athaliana/all_NLR.gtf
+	echo 'finished ${BASENAME}' 
+done
 echo 'finished!' 
