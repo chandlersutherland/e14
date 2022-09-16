a6a24a19d2995dcf226fb80a7c51ad3d3a95577e
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-09-16 05:59:00 -0700

    run for just one to see what's up

--- htseq_counts.bash
+++ htseq_counts.bash
@@ -19,10 +19,11 @@ OUTPUT=/global/scratch/users/chandlersutherland/e14/STAR_output/htseq_count
 
 cd $INPUT 
 
-for f in *.bam
-do 
-	BASENAME=$(basename $f .out.bam) 
+#for f in *.bam
+f='SRR17281233_Aligned.out.bam'
+#do 
+	BASENAME=$(basename $f _Aligned.out.bam) 
 	htseq-count -r pos -s yes -c $OUTPUT/$BASENAME.txt $f /global/scratch/users/chandlersutherland/Athaliana/Araport11_GTF_genes_transposons.current.gtf
 
-done
+#done
 echo 'finished!' 
