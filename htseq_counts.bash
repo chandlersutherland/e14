8d1dc12b3065a28bfc480327a568a6039263ee96
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-09-16 09:36:33 -0700

    finish after timeout

--- htseq_counts.bash
+++ htseq_counts.bash
@@ -4,7 +4,7 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=03:00:00
+#SBATCH --time=01:00:00
 #SBATCH --mail-user=chandlersutherland@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
@@ -19,10 +19,11 @@ OUTPUT=/global/scratch/users/chandlersutherland/e14/STAR_output/htseq_count
 
 cd $INPUT 
 
-for f in *.bam
-do 
+f='SRR3465232_Aligned.out.bam'
+#for f in *.bam
+#do 
 	BASENAME=$(basename $f _Aligned.out.bam) 
 	htseq-count -r pos -s yes -c $OUTPUT/$BASENAME.tsv $f /global/scratch/users/chandlersutherland/Athaliana/GTFs/all_NLRs.gtf
-	echo 'finished ${BASENAME}' 
-done
+	echo 'finished' ${BASENAME} 
+#done
 echo 'finished!' 
