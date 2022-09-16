6056c29512c1bf20187c92cc9182de085c6f9be2
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-09-15 22:41:57 -0700

    some data

---
+++ htseq_counts.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=htseq_counts 
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=01:00:00
+#SBATCH --mail-user=chandlersutherland@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
+#SBATCH --output=/global/home/users/chandlersutherland/slurm_stdout/slurm-%j.out
+
+module load python 
+source activate e14
+
+INPUT=/global/scratch/users/chandlersutherland/e14/STAR_output/sort_index
+OUTPUT=/global/scratch/users/chandlersutherland/e14/STAR_output/htseq_count
+
+
+cd $INPUT 
+
+for f in *.bam
+do 
+	BASENAME=$(basename $f .out.bam) 
+	htseq-count -r pos -s no \
+		-c $OUTPUT/$BASENAME.txt 
+		$f \
+		/global/scratch/users/chandlersutherland/Athaliana/Araport11_GTF_genes_transposons.current.gtf
+
+done
+echo 'finished!' 
