9b9f00721046478c1fd9be6900a8c6beac0662f6
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-09-17 10:33:53 -0700

    python goddess

---
+++ methylation_coverage.py
@@ -0,0 +1,41 @@
+import os 
+import pandas as pd 
+import glob 
+import numpy as np 
+
+#takes a directory path as an argument, and will evaluate all the txt files within for NLR methylation patterns 
+directory=str(sys.argv[1])
+files = glob.glob(os.path.join(directory, "*.txt"))
+positions=pd.read_csv('/global/home/users/chandlersutherland/e14/data/all_NLR.bed', sep='\t', header=0, names=['Chromosome', 'start', 'end', 'gene', 'strand'], index_col=False)
+#define output path!
+
+dfs = []
+for f in files:
+    print('Location:', f)
+    context=f.split('/')[-1].split('_')[0]
+    accession=f.split('_')[2]
+    print('Context:', context, 'Accession:', accession)
+    
+    df=pd.read_csv(f, skiprows=1, names=['read', 'strand', 'chrom', 'position', 'context'], sep='\t')
+    df['accession']=accession
+    df['context']=context 
+    df['NLR']= np.nan
+    dfs.append(df)
+
+def methylation_counter(extraction, positions):
+    for j in range(0, len(extraction)):
+        for i in range(0, len(positions)):
+            if extraction.iloc[j,2] == positions.iloc[i,0]:
+                if positions.iloc[i,1] <= extraction.iloc[j,3] <= positions.iloc[i,2]:
+                    #print('methylation match found')
+                    extraction.iloc[j,6] = positions.iloc[i,3]
+    
+    return extraction.dropna()
+                 
+
+all = [methylation_counter(i, positions) for i in dfs]
+concat = pd.concat(all).sort_values(['accession', 'context', 'HV'])
+concat[['NLR', 'HV']] = test_concat.NLR.str.split('_', expand = True)
+
+concat.to_csv(os.path.join(directory, '/summary_reports/all_NLR_cytosines.txt'), sep="\t")
+
