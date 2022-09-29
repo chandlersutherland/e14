207ec576c0ce1109fe003ece1b4332f965c0b4b6
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-09-29 09:25:55 -0700

    bash to run simulate_experiment

---
+++ simulate_experiment_runner.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=simulate_experiment_runner
+#SBATCH --account=ac_kvkallow
+#SBATCH --partition=savio
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=01:00:00
+#SBATCH --mail-user=chandlersutherland@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
+#SBATCH --output=/global/home/users/chandlersutherland/slurm_stdout/slurm-%j.out
+
+module load r
+module load r-packages
+
+R /global/home/users/chandlersutherland/e14/simulate_experiment.r
