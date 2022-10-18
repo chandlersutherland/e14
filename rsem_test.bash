a6c96b66015fcd5a38f1f1d4e65e5b9663475445
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-10-18 05:05:29 -0700

    come on

--- rsem_test.bash
+++ rsem_test.bash
@@ -4,7 +4,7 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=00:40:00
+#SBATCH --time=04:00:00
 #SBATCH --mail-user=chandlersutherland@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
@@ -20,6 +20,8 @@ GTF_FILE=/global/scratch/users/chandlersutherland/Athaliana/GTFs/Araport11_GTF_g
 FASTQ_FILE=/global/scratch/users/chandlersutherland/e14/rna_fastq_files/williams/SRR17281233.fastq
 GFF3=/global/scratch/users/chandlersutherland/phytozome/Athaliana/Araport11/assembly/Athaliana_447_Araport11.gene_exons.gff3
 
-rsem-prepare-reference --gff3 $GFF3 --bowtie --bowtie-path ~/.conda/envs/e14/bin $ARAPORT11_FA araport11
+#rsem-prepare-reference --gff3 $GFF3 --bowtie --bowtie-path ~/.conda/envs/e14/bin $ARAPORT11_FA araport11
 
+cd /global/scratch/users/chandlersutherland/phytozome/Athaliana/Araport11/assembly/
 rsem-calculate-expression $FASTQ_FILE araport11 SRR17281233 
+	
