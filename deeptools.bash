e955a1e0f2f21352b3068a5e54c71c09a2fd1188
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-10-27 12:40:27 -0700

    two functions

--- deeptools.bash
+++ deeptools.bash
@@ -12,53 +12,58 @@
 
 module load parallel 
 
-plotter() {
+#input is a file path 
+bed_to_big() {
 	module load python/3.8.8
 	source activate e14_deeptools
-	#first, need to remove the track line from the bedgraph files, and save them as *.fixed.bed 
-	INPUT_DIR=/global/scratch/users/chandlersutherland/e14/deeptools/cpg_highcov
-	#grep -v track ${INPUT_DIR}/${1}.bed > ${INPUT_DIR}/${1}.fixed.bed
-
-	#now, convert to bigwig 
+	
 	CHROM_SIZE=$SCRATCH/e14/deeptools/Athaliana_447_TAIR10.genome.sizes
-	mkdir -p $SCRATCH/e14/deeptools/cpg_highcov/bigwig/
-	sort -k1,1 -k2,2n ${INPUT_DIR}/${1}_cpg_highcov.bed > ${INPUT_DIR}/${1}_cpg_highcov_sorted.bed
-	bedGraphToBigWig ${INPUT_DIR}/${1}_cpg_highcov_sorted.bed $CHROM_SIZE $SCRATCH/e14/deeptools/cpg_highcov/bigwig/${1}.bw
+	DIR=$(dirname $(readlink -f ${1}))
+	BW_DIR=${DIR}/bigwig; mkdir -p $BW_DIR
+	
+	#first, need to remove the track line from the bedgraph files, and save them as *.fixed.bed 
+	grep -v track ${1} |
+	sort -k1,1 -k2,2n > ${1}_sorted.bed
+	
+	#now, convert to bigwig 
+	bedGraphToBigWig ${1}_sorted.bed $CHROM_SIZE $BW_DIR
 	
 	echo 'converted ${1} to bigwig' 
 	
 	conda deactivate
 	module purge 
+}
 
+#input is a bw file 
+plotter(){
 	#ok, now to do some profile plots 
 	module load python 
 	source activate e14 
 
-	density_file= $SCRATCH/e14/deeptools/cpg_highcov/bigwig/${1}.bw
+	density_file= ${1}
 	THREADS=$SLURM_NTASKS
 	HV_BED=/global/home/users/chandlersutherland/e14/data/hv_NLR.bed
 	NONHV_BED=/global/home/users/chandlersutherland/e14/data/nonhv_NLR2.bed
-	DEEPTOOLS_DIR=$SCRATCH/e14/deeptools/cpg_highcov
+	DIR=$(dirname $(readlink -f ${1}))
+	BASENAME=$(basename ${1})
+	DEEPTOOLS_DIR=${DIR}/deeptools_out
 	mkdir -p $DEEPTOOLS_DIR
 	
 	#not currently working on interactive mode, but no error messages?
-	computeMatrix scale-regions -S $SCRATCH/e14/deeptools/cpg_highcov/bigwig/${1}.bw \
+	computeMatrix scale-regions -S $density_file \
 		-R ${HV_BED} ${NONHV_BED} \
 		--regionBodyLength 4000 \
-		-o ${DEEPTOOLS_DIR}/${1}.both.mat.gz
+		-o ${DEEPTOOLS_DIR}/${BASENAME}.both.mat.gz
 	
-	plotProfile -m ${DEEPTOOLS_DIR}/${1}.both.mat.gz \
-		-out ${DEEPTOOLS_DIR}/${1}.both.pdf \
+	plotProfile -m ${DEEPTOOLS_DIR}/${BASENAME}.both.mat.gz \
+		-out ${DEEPTOOLS_DIR}/${BASENAME}.both.pdf \
 		--numPlotsPerRow 2 \
 		--plotTitle "${1}" \
-		--outFileNameData ${DEEPTOOLS_DIR}/${1}.both.tab 
+		--outFileNameData ${DEEPTOOLS_DIR}/${BASENAME}.both.tab 
 		
-	echo 'finished' ${1}
+	echo 'finished' ${BASENAME}
 }
 
 export -f plotter
-
-BISULFITE='SRR17281087 SRR17281086 SRR17281088 SRR17281085'
-parallel plotter ::: $BISULFITE
-
+export -f bed_to_big
 echo 'parallel finished'
