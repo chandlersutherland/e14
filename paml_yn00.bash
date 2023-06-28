db938623c4fefe6254092bdd8a10cd075697cd38
Chandler Sutherland <chandlersutherland@berkeley.edu>
2023-06-28 08:50:22 -0700

    running codeml

--- paml_yn00.bash
+++ paml_yn00.bash
@@ -4,7 +4,7 @@
 #SBATCH --qos=minium_htc4_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=12:00:00
+#SBATCH --time=72:00:00
 #SBATCH --mail-user=chandlersutherland@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
@@ -17,34 +17,34 @@ source activate e14
 base=/global/scratch/users/chandlersutherland/e14/popgen/clades
 output_csv=/global/scratch/users/chandlersutherland/e14/popgen/paml.csv
 
-echo "Clade,S,N,dS_NG,dN_NG,dS_ML,dN_ML" > $output_csv
+#echo "Clade,S,N,dS_NG,dN_NG,dS_ML,dN_ML" > $output_csv
 
 #for each clade, identify the codon alignment file and tree, then run hyphy to calculate dn/ds
 while read clade
 do 
-	#actually run yn00
+	#actually run codeml
 	cd ${base}/${clade}
-	codeml codeml.ctl 
+	codeml codeml_m0.ctl 
 	echo "finished codeml $clade"
 done < /global/scratch/users/chandlersutherland/e14/popgen/clades.txt
 
 #do separately bc there seems to be a lag 
-while read clade
-do
+#while read clade
+#do
     #Get the number of synonymous and nonsynonymous sites, should be the same for both estimates
-    cd ${base}/${clade}
-    S=$(cat 2ML.dS | grep -Eo '[+-]?[0-9]+\.[0-9]+' | wc -l)
-    N=$(cat 2ML.dN | grep -Eo '[+-]?[0-9]+\.[0-9]+' | wc -l)
+#    cd ${base}/${clade}
+ #   S=$(cat 2ML.dS | grep -Eo '[+-]?[0-9]+\.[0-9]+' | wc -l)
+  #  N=$(cat 2ML.dN | grep -Eo '[+-]?[0-9]+\.[0-9]+' | wc -l)
     
     #get the average NG dS and dN
-    dS_NG=$(cat 2NG.dS | grep -Eo '[+-]?[0-9]+\.[0-9]+' | awk '{a+=$1} END{print a/NR}')
-    dN_NG=$(cat 2NG.dN | grep -Eo '[+-]?[0-9]+\.[0-9]+' | awk '{a+=$1} END{print a/NR}')
+#    dS_NG=$(cat 2NG.dS | grep -Eo '[+-]?[0-9]+\.[0-9]+' | awk '{a+=$1} END{print a/NR}')
+ #   dN_NG=$(cat 2NG.dN | grep -Eo '[+-]?[0-9]+\.[0-9]+' | awk '{a+=$1} END{print a/NR}')
 
     #get the average ML dS and dN
-    dS_ML=$(cat 2ML.dS | grep -Eo '[+-]?[0-9]+\.[0-9]+' | awk '{a+=$1} END{print a/NR}')
-    dN_ML=$(cat 2ML.dN | grep -Eo '[+-]?[0-9]+\.[0-9]+' | awk '{a+=$1} END{print a/NR}')
+  #  dS_ML=$(cat 2ML.dS | grep -Eo '[+-]?[0-9]+\.[0-9]+' | awk '{a+=$1} END{print a/NR}')
+   # dN_ML=$(cat 2ML.dN | grep -Eo '[+-]?[0-9]+\.[0-9]+' | awk '{a+=$1} END{print a/NR}')
 
     #write to csv with clade info
-    echo "${clade},${S},${N},${dS_NG},${dN_NG},${dS_ML},${dN_ML}" >> $output_csv
-    echo "finished $clade"
-done < /global/scratch/users/chandlersutherland/e14/popgen/clades.txt 
+    #echo "${clade},${S},${N},${dS_NG},${dN_NG},${dS_ML},${dN_ML}" >> $output_csv
+    #echo "finished $clade"
+#done < /global/scratch/users/chandlersutherland/e14/popgen/clades.txt 
