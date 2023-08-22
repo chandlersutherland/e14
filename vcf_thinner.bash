2529b1b54b72cd0018a992809caaf79950375333
Chandler Sutherland <chandlersutherland@berkeley.edu>
2023-08-21 18:45:17 -0700

    all chromosomes

--- vcf_thinner.bash
+++ vcf_thinner.bash
@@ -13,6 +13,11 @@
 module load vcftools
 cd /global/scratch/users/chandlersutherland/e14/popgen/vcf_1001
 
-vcftools --vcf 1001genomes_snp-short-indel_only_ACGTN.vcf --keep nlrome_IDs.txt --recode --recode-INFO-all --out nlrome_partial 
+#vcftools --vcf 1001genomes_snp-short-indel_only_ACGTN.vcf --keep nlrome_IDs.txt --recode --recode-INFO-all --out nlrome_partial 
+bed='/global/scratch/users/chandlersutherland/Athaliana/Atha_cds.bed'
+#vcftools --vcf nlrome_partial.recode.vcf --chr 1 --recode --bed $bed nlrome_partial_1_cds 
 
-
+vcftools --vcf nlrome_partial.recode.vcf --chr 2 --recode --bed $bed nlrome_partial_2_cds
+vcftools --vcf nlrome_partial.recode.vcf --chr 3 --recode --bed $bed nlrome_partial_3_cds
+vcftools --vcf nlrome_partial.recode.vcf --chr 4 --recode --bed $bed nlrome_partial_4_cds
+vcftools --vcf nlrome_partial.recode.vcf --chr 5 --recode --bed $bed nlrome_partial_5_cds
