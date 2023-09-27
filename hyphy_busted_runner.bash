2133e8fe3651e2fa347c2dda8880bc336c0d59d0
Chandler Sutherland <chandlersutherland@berkeley.edu>
2023-09-26 19:47:41 -0700

    parallel lifestyle

--- hyphy_busted_runner.bash
+++ hyphy_busted_runner.bash
@@ -11,31 +11,55 @@
 #SBATCH --output=/global/home/users/chandlersutherland/slurm_stdout/slurm-%j.out
 
 module load python 
+module load parallel
 source activate e14 
 
 base=/global/scratch/users/chandlersutherland/e14/popgen/clades
-output_csv=/global/scratch/users/chandlersutherland/e14/popgen/hyphy_busted_p.csv
+output_csv=/global/scratch/users/chandlersutherland/e14/popgen/hyphy_busted_internal_p.csv
+clade=$(cat /global/scratch/users/chandlersutherland/e14/popgen/clades.txt)
+
+echo "Clade,p" > $output_csv
+
+BUSTED_RUN(){
+	alignment=${base}/${1}/popgenome/${1}.pal2nal.fas
+	tree=${base}/${1}/RAxML*.out
+	log_file=${base}/${1}/hyphy_busted_internal.log
+	
+	echo "Running Hyphy on $1"
+	hyphy  busted CPU=24 --alignment ${alignment} --tree ${tree} --branches Internal | tee -a ${log_file}
+	
+	p=$(grep 'Likelihood ratio test' $log_file | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+	
+	echo "${1},${p}" >> $output_csv
+	echo "finished $1"
+	
+}
+
+export base=$base
+export output_csv=$output_csv 
+export -f BUSTED_RUN
+
+parallel BUSTED_RUN ::: $clade 
 
 #echo "Clade,p" > $output_csv
 
 #for each clade, identify the codon alignment file and tree, then run hyphy busted
-while read clade
-do 
+#while read clade
+#do 
 	#name inputs 
-	alignment=${base}/${clade}/popgenome/${clade}.pal2nal.fas
-	tree=${base}/${clade}/RAxML*.out
+	#alignment=${base}/${clade}/popgenome/${clade}.pal2nal.fas
+	#tree=${base}/${clade}/RAxML*.out
 	
-	log_file=${base}/${clade}/hyphy_busted_internal.log
-	echo "Running Hyphy on $clade"
-	
+#	log_file=${base}/${clade}/hyphy_busted_internal.log
+#	echo "Running Hyphy on $clade"
+#	
 	#actually run hyphy in multithreading mode to hopefully speed up 
-	hyphy  busted CPU=24 --alignment ${alignment} --tree ${tree} --branches Internal | tee -a $log_file
+#	hyphy  busted CPU=24 --alignment ${alignment} --tree ${tree} --branches Internal | tee -a $log_file
 	
 	
 	#parse out p value  
-	p=$(grep 'Likelihood ratio test' $log_file | grep -Eo '[+-]?[0-9]+([.][0-9]+)?') 
+#	p=$(grep 'Likelihood ratio test' $log_file | grep -Eo '[+-]?[0-9]+([.][0-9]+)?') 
 	
 	#write to csv with clade info 
-	echo "${clade},${p}" >> $output_csv
-	echo "finished $clade"
-done < /global/scratch/users/chandlersutherland/e14/popgen/clades3.txt 
+##	echo "finished $clade"
+#done < /global/scratch/users/chandlersutherland/e14/popgen/clades3.txt 
